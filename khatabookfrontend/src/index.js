81fc4ac02f41464f5b1b90b0cae0b1d7050ace60
Pranjal Tiwari <pranjalt363@gmail.com>
2024-12-16 18:03:47 +0530

    updates

--- khatabookfrontend/src/index.js
+++ khatabookfrontend/src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./context/AuthContext";
-import {
-  NotificationAPIProvider,
-  NotificationPopup,
-} from "@notificationapi/react";
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
