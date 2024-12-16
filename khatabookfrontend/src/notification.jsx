61ee912ba21064ddd4a0ff5fd4dc14c39584721f
Pranjal Tiwari <pranjalt363@gmail.com>
2024-12-16 18:10:18 +0530

    updates

--- khatabookfrontend/src/notification.jsx
+++
@@ -1,7 +0,0 @@
-import NotificationAPI from "notificationapi-js-client-sdk";
-import "notificationapi-js-client-sdk/dist/styles.css";
-const notificationapi = new NotificationAPI({
-  clientId: "pranjalt363@gmail.com",
-  userId: "pranjal4tiwari@gmail.com",
-});
-notificationapi.askForWebPushPermission();
