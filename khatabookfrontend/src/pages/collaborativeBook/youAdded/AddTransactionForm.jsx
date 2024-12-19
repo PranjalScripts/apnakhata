e10868b0161c3b68c9a39d42a34055e1adca9497
Pranjal Tiwari <pranjalt363@gmail.com>
2024-12-19 10:12:17 +0530

    updates

--- khatabookfrontend/src/pages/collaborativeBook/youAdded/AddTransactionForm.jsx
+++ khatabookfrontend/src/pages/collaborativeBook/youAdded/AddTransactionForm.jsx
@@ -63,7 +63,7 @@ const AddTransactionForm = ({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-gray-900 bg-opacity-60 backdrop-blur-sm flex items-center justify-center z-50 px-4"
+            className="fixed inset-0 bg-gray-900 bg-opacity-60 backdrop-blur-sm flex items-center justify-center z-[9999] px-4"
             onClick={() => setShowForm(false)}
           >
             <motion.div
